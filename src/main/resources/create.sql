caac82c7795a5fe996c37e848a7ccb5fe3ccec0b
imashevdt11 <imashevdt@gmail.com>
2024-05-12 10:50:36 +0600

    changed length for password in database

--- src/main/resources/create.sql
+++ src/main/resources/create.sql
@@ -4,7 +4,7 @@ CREATE TABLE users
     id       BIGINT PRIMARY KEY DEFAULT NEXTVAL('user_id_seq'),
     email    VARCHAR(255) NOT NULL UNIQUE,
     username VARCHAR(50)  NOT NULL UNIQUE,
-    password VARCHAR(50)  NOT NULL,
+    password VARCHAR(255)  NOT NULL,
     enabled  BOOLEAN            DEFAULT FALSE
 );
 
